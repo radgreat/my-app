c8b999c30a1ec8f8a7ef25ed813f02d8c6e5a03f
rad <radgreat@gmail.com>
2025-09-29 17:57:03 +0800

    initial commit

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,8 @@ const bcrypt = require('bcrypt');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback'
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
+  //callbackURL: '/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     const email = profile.emails[0].value;
